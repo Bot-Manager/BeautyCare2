7916e693155806a9f73d3c46b9cf7c94ba267325
AlexRpg-86 <54549313+AlexRpg-86@users.noreply.github.com>
2019-11-16 01:19:03 -0300

    Añadí Home

--- BeautyCare/urls.py
+++ BeautyCare/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path , include
 from django.conf import settings
 from django.conf.urls.static import static
+from servicios.views import mostrarHome
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('' , mostrarHome),
     path('servicios/' , include('servicios.urls' , namespace='servicios')),
     path('reservas/' , include('reservas.urls' , namespace='reservas')),
 ]
