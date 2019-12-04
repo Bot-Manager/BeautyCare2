86a0e780898ee68633bba2a16153d25a321e6ec6
Denisse Lyon <denisse.lyon@gmail.com>
2019-12-03 22:03:02 -0300

    ok

--- galerias/admin.py
+++ galerias/admin.py
@@ -4,6 +4,13 @@ from django.contrib import admin
 
 from .models import Galerias , Categoria
 
+class Lista(admin.ModelAdmin):
+    list_display = ['nombre', 'descripcion', 'imagen','categoria']
+    search_fields = ['nombre', 'categoria']
+    list_filter = ('nombre', 'categoria')
 
-admin.site.register(Galerias)
+
+
+
+admin.site.register(Galerias,Lista)
 admin.site.register(Categoria)
